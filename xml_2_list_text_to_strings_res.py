5fefb47b13296e88b2fc21c3e98baaa222cb86bc
Kukuh <kukuhsain@gmail.com>
2016-07-28 13:06:01 +0700

    Update xml_2 script to support text, hint, digits, and title in both layout and menu directory

--- xml_2_list_text_to_strings_res.py
+++ xml_2_list_text_to_strings_res.py
@@ -1,9 +1,11 @@
 import os
+import re
 from string import punctuation
 from settings import ROOT_PATH
 
 RES_PATH = '/app/src/main/res'
-LAYOUT_PATH = '/layout'
+# LAYOUT_PATH = '/layout'
+LAYOUT_PATH = ''
 STRINGS_PATH = '/values/strings'
 
 print ROOT_PATH
@@ -12,42 +14,65 @@ PATH = ROOT_PATH + RES_PATH + LAYOUT_PATH
 def strip_punctuation(s):
     return ''.join(c for c in s if c not in punctuation)
 
+def list_realstrings_to_file(line, filter_result, filename_result):
+    if "@string" not in line:
+        string_name_in_xml = strip_punctuation(filter_result.lower()).replace(' ', '_')
+        
+        prefix = '<string name="' + filename_result + '_' + string_name_in_xml + '">'
+        suffix = '</string>\n'
+        
+        final_result = prefix + filter_result + suffix
+        return final_result
+    else:
+        return False
+
 temp_file = []
 for root, dirs, files in os.walk(PATH):
     for single_file in sorted(files):
-        fullpath = os.path.join(PATH, single_file)
+        fullpath = os.path.join(root, single_file)
         # print fullpath
         file_name = single_file.replace('.xml', '')
-        
-        starting_mark = "<!--"+file_name+" start-->\n"
-        temp_file.append(starting_mark)
 
-        with open(fullpath, 'r') as f:
-            for line in f.readlines():
-                if "android:text=" in line:
-                    if "@string" in line:
-                        break
-                    whitespaces_removed = line.replace("  ", "")
-                    prefixes_removed = whitespaces_removed.replace('android:text="', "")
-                    suffixes_removed = prefixes_removed.replace('"\n', "")
-                    final_text = suffixes_removed
+        if "layout" in root or "menu" in root:
+            print file_name
+            tag = file_name.split('_', 1)[0]
+            filtered_file_name = file_name.split('_', 1)[1]
+            filename_result = filtered_file_name + '_' + tag
+            
+            starting_mark = "<!--"+filename_result+" start-->\n"
+            temp_file.append(starting_mark)
 
-                    string_name_in_xml = strip_punctuation(final_text.lower()).replace(' ', '_')
+            with open(fullpath, 'r') as f:
+                for line in f.readlines():
+                    if "android:text=" in line:
+                        filter_result = re.findall('text="(.*)"', line)[0]
+                        final_result = list_realstrings_to_file(line, filter_result, filename_result)
+                        if final_result:
+                            temp_file.append(final_result)
 
-                    tag = file_name.split('_', 1)[0]
-                    filtered_file_name = file_name.split('_', 1)[1]
-                    filename_result = filtered_file_name + '_' + tag
+                    elif "android:hint" in line:
+                        filter_result = re.findall('hint="(.*)"', line)[0]
+                        final_result = list_realstrings_to_file(line, filter_result, filename_result)
+                        if final_result:
+                            temp_file.append(final_result)
                     
-                    prefix = '<string name="' + filename_result + '_' + string_name_in_xml + '">'
-                    suffix = '</string>\n'
-                    
-                    final_result = prefix + final_text + suffix
-                    temp_file.append(final_result)
-        ending_mark = "<!--"+file_name+" end-->\n"
-        temp_file.append(ending_mark)
+                    elif "android:digits" in line:
+                        filter_result = re.findall('digits="(.*)"', line)[0]
+                        final_result = list_realstrings_to_file(line, filter_result, filename_result)
+                        if final_result:
+                            temp_file.append(final_result)
 
-        new_line = "\n"
-        temp_file.append(new_line)
+                    elif "android:title" in line:
+                        filter_result = re.findall('title="(.*)"', line)[0]
+                        final_result = list_realstrings_to_file(line, filter_result, filename_result)
+                        if final_result:
+                            temp_file.append(final_result)
+
+            ending_mark = "<!--"+filename_result+" end-->\n"
+            temp_file.append(ending_mark)
+
+            new_line = "\n"
+            temp_file.append(new_line)
 
 print(temp_file)
 
